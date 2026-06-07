01101bbf84f9e7c685f02db98e663f320c160f63
josemateus <josemateus.ufma@gmail.com>
2026-06-07 14:37:33 -0300

    Anotacoes

---
+++ src/main/java/io/mateusjose98/annotations/CPOST.java
@@ -0,0 +1,13 @@
+package io.mateusjose98.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CPOST {
+  public String value() default "";
+  
+}
